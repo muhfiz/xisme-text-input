3976cc6db13cb621f93ff9faf27f1f1ce3d11220
Muhamad Hafiz <muhamadhafiz2508@gmail.com>
2021-08-24 00:10:15 +0700

    adding resources

--- InputTextEditor/src/main/java/com/xisme/inputtexteditor/XismeTextInputLayout.kt
+++ InputTextEditor/src/main/java/com/xisme/inputtexteditor/XismeTextInputLayout.kt
@@ -3,6 +3,9 @@ package com.xisme.inputtexteditor
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class XismeTextInputLayout: ConstraintLayout {
@@ -10,7 +13,27 @@ class XismeTextInputLayout: ConstraintLayout {
     constructor(context: Context): super(context)
 
     constructor(context: Context, attrs: AttributeSet): super(context, attrs){
-        Log.d(TAG, ": ${getChildAt(0)}")
+        Log.d(TAG, "created: ")
+    }
+
+    override fun onFinishInflate() {
+        Log.d(TAG, "onFinishInflate: onFinish")
+        if (childCount > 1)
+            throw IllegalArgumentException("Only 1 child allowed");
+        
+        if (getChildAt(0) !is XismeTextInputEditText)
+            throw IllegalArgumentException("Child must be a XismeTextInputEditText");
+
+        addingTheToolsView()
+        super.onFinishInflate()
+    }
+
+    private fun addingTheToolsView(){
+        var view = AppCompatImageView(context).apply {
+            id = View.generateViewId()
+        }
+
+        view
     }
 }
 
